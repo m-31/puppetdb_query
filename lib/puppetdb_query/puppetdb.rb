f645d788397f1931e8e3289a207039eb1f64dd41
m31@mymble.moominland <search@meyling.com>
2016-06-25 23:57:14 +0200

    tune update methods

--- lib/puppetdb_query/puppetdb.rb
+++ lib/puppetdb_query/puppetdb.rb
@@ -3,36 +3,81 @@ require 'json'
 
 module PuppetDBQuery
   class PuppetDB
-    NODES = "/v4/nodes"
+    NODES = "/v3/nodes"
+    FACTS = "/v3/facts"
 
-    def initialize(host = HOST, port = 443, protocol = "https", nodes = NODES)
-      @url = "#{protocol}://#{host}:#{port}#{nodes}"
+    def initialize(host = HOST, port = 443, protocol = "https", nodes = NODES, facts = FACTS)
+      @nodes_url = "#{protocol}://#{host}:#{port}#{nodes}"
+      @facts_url = "#{protocol}://#{host}:#{port}#{facts}"
       @lock = Mutex.new
     end
 
     def nodes
       @lock.synchronize do
-        uri = URI.parse(@url)
+        uri = URI.parse(@nodes_url)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = uri.scheme == 'https'
         http.read_timeout = 10
         request = Net::HTTP::Get.new(uri.request_uri)
         request['Accept'] = "application/json"
         response = http.request(request)
-        JSON.parse(response.body).map { |data| data['certname'] }
+        # TODO in '/v3/nodes' we must take 'certname'
+        JSON.parse(response.body).map { |data| data['name'] }
+      end
+    end
+
+    def nodes_update_facts_since(timestamp)
+      @lock.synchronize do
+        ts = timestamp
+        ts = Time.iso8601(ts) if ts.is_a?(String)
+        uri = URI.parse(@nodes_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.scheme == 'https'
+        http.read_timeout = 10
+        request = Net::HTTP::Get.new(uri.request_uri)
+        request['Accept'] = "application/json"
+        response = http.request(request)
+        json = JSON.parse(response.body)
+        json.delete_if { |data| Time.iso8601(data["facts_timestamp"]) < ts }
+        # TODO in '/v3/nodes' we must take 'certname'
+        json.delete_if { |data| data["facts_timestam"] }.map { |data| data['name'] }
       end
     end
 
     def node_facts(node)
       @lock.synchronize do
-        uri = URI.parse("#{@url}/#{node}/facts")
+        uri = URI.parse("#{@nodes_url}/#{node}/facts")
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = uri.scheme == 'https'
         http.read_timeout = 10
         request = Net::HTTP::Get.new(uri.request_uri)
         request['Accept'] = "application/json"
         response = http.request(request)
-        Hash[JSON.parse(response.body).map { |data| [data["name"], data["value"]] }]
+        json = JSON.parse(response.body)
+        return nil if json.include?("error")
+        Hash[json.map { |data| [data["name"], data["value"]] }]
+      end
+    end
+
+    # get all facts
+    def facts
+      @lock.synchronize do
+        uri = URI.parse(@facts_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.scheme == 'https'
+        http.read_timeout = 60
+        request = Net::HTTP::Get.new(uri.request_uri)
+        request['Accept'] = "application/json"
+        response = http.request(request)
+        json = JSON.parse(response.body)
+        # json.each { |fact| pp fact }
+        result = {}
+        json.each do |fact|
+          data = result[fact["certname"]]
+          result[fact["certname"]] = data = {} unless data
+          data[fact["name"]] = fact["value"]
+        end
+        result
       end
     end
   end
--- lib/puppetdb_query/updater.rb
+++ lib/puppetdb_query/updater.rb
@@ -9,6 +9,7 @@ module PuppetDBQuery
       @destination = destination
     end
 
+    # 335.6 seconds: update time for 1561 nodes
     def update
       source_nodes = source.nodes
       destination_nodes = destination.nodes
@@ -19,9 +20,43 @@ module PuppetDBQuery
         begin
           destination.node_update(node, source.node_facts(node))
         rescue
+          STDERR.puts $!  # TODO logger
+        end
+      end
+    end
+
+    # 166.4 seconds: update time for 1561 nodes
+    def update2
+      source_nodes = source.nodes
+      destination_nodes = destination.nodes
+      (destination_nodes - source_nodes).each do |node|
+        destination.node_delete(node)
+      end
+      complete = source.facts
+      complete.each do |node, facts|
+        begin
+          destination.node_update(node, facts)
+        rescue
           STDERR.puts $!
         end
       end
+    end
+
+    def update3(last_update_timestamp)
+      source_nodes = source.nodes
+      destination_nodes = destination.nodes
+      (destination_nodes - source_nodes).each do |node|
+        destination.node_delete(node)
+      end
+      modified = source.nodes_update_facts_since(last_update_timestamp)
+      modified.each do |node|
+        begin
+          destination.node_update(node, source.node_facts(node))
+        rescue
+          STDERR.puts $!  # TODO logger
+        end
+      end
+      modified.size
     end
   end
 end
