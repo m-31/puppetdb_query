ef2162f1980bbf3675ffa4a2e2530d2884265a2f
m31@mymble.moominland <search@meyling.com>
2016-10-01 20:03:47 +0200

    refactor access to puppet db web api

--- lib/puppetdb_query/puppetdb.rb
+++ lib/puppetdb_query/puppetdb.rb
@@ -1,9 +1,13 @@
 require 'net/http'
 require 'json'
 
+require_relative 'logging'
+
 module PuppetDBQuery
   # access puppetdb data
   class PuppetDB
+    include Logging
+
     NODES = "/v4/nodes".freeze
     FACTS = "/v4/facts".freeze
 
@@ -14,71 +18,56 @@ module PuppetDBQuery
     end
 
     def nodes
-      @lock.synchronize do
-        uri = URI.parse(@nodes_url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = uri.scheme == 'https'
-        http.read_timeout = 10
-        request = Net::HTTP::Get.new(uri.request_uri)
-        request['Accept'] = "application/json"
-        response = http.request(request)
-        # TODO: in '/v3/nodes' we must take 'name'
-        JSON.parse(response.body).map { |data| data['certname'] }
-      end
+      # TODO: in '/v3/nodes' we must take 'name'
+      api_nodes.map { |data| data['certname'] }
     end
 
     def nodes_update_facts_since(timestamp)
-      @lock.synchronize do
-        ts = timestamp
-        ts = Time.iso8601(ts) if ts.is_a?(String)
-        uri = URI.parse(@nodes_url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = uri.scheme == 'https'
-        http.read_timeout = 10
-        request = Net::HTTP::Get.new(uri.request_uri)
-        request['Accept'] = "application/json"
-        response = http.request(request)
-        json = JSON.parse(response.body)
-        json.delete_if { |data| Time.iso8601(data["facts_timestamp"]) < ts }
-        # TODO: in '/v3/nodes' we must take 'name'
-        json.delete_if { |data| data["facts_timestamp"] }.map { |data| data['certname'] }
-      end
+      ts = timestamp
+      ts = Time.iso8601(ts) if ts.is_a?(String)
+      json = api_nodes
+      json.delete_if { |data| Time.iso8601(data["facts_timestamp"]) < ts }
+      # TODO: in '/v3/nodes' we must take 'name'
+      json.delete_if { |data| data["facts_timestamp"] }.map { |data| data['certname'] }
     end
 
     def node_facts(node)
-      @lock.synchronize do
-        uri = URI.parse("#{@nodes_url}/#{node}/facts")
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = uri.scheme == 'https'
-        http.read_timeout = 10
-        request = Net::HTTP::Get.new(uri.request_uri)
-        request['Accept'] = "application/json"
-        response = http.request(request)
-        json = JSON.parse(response.body)
-        return nil if json.include?("error")
-        Hash[json.map { |data| [data["name"], data["value"]] }]
-      end
+      json = get_json("#{@nodes_url}/#{node}/facts", 10)
+      return nil if json.include?("error")
+      Hash[json.map { |data| [data["name"], data["value"]] }]
     end
 
     # get all facts
     def facts
+      json = get_json(@facts_url, 60)
+      # json.each { |fact| pp fact }
+      result = {}
+      json.each do |fact|
+        data = result[fact["certname"]]
+        result[fact["certname"]] = data = {} unless data
+        data[fact["name"]] = fact["value"]
+      end
+      result
+    end
+
+    private
+
+    def api_nodes
+      get_json(@nodes_url, 10)
+    end
+
+    def get_json(url, timeout)
       @lock.synchronize do
-        uri = URI.parse(@facts_url)
+        logger.info "get json from #{url}"
+        uri = URI.parse(url)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = uri.scheme == 'https'
-        http.read_timeout = 60
+        http.read_timeout = timeout
         request = Net::HTTP::Get.new(uri.request_uri)
         request['Accept'] = "application/json"
         response = http.request(request)
-        json = JSON.parse(response.body)
-        # json.each { |fact| pp fact }
-        result = {}
-        json.each do |fact|
-          data = result[fact["certname"]]
-          result[fact["certname"]] = data = {} unless data
-          data[fact["name"]] = fact["value"]
-        end
-        result
+        logger.info "  got #{response.body.size} characters from #{url}"
+        JSON.parse(response.body)
       end
     end
   end
