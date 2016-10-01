6b4508d7b87c279df06a172c942e1f8c56c82ae6
m31@mymble.moominland <search@meyling.com>
2016-10-01 21:36:13 +0200

    refactored puppetdb nodes with facts updates since

--- lib/puppetdb_query/puppetdb.rb
+++ lib/puppetdb_query/puppetdb.rb
@@ -17,30 +17,46 @@ module PuppetDBQuery
       @lock = Mutex.new
     end
 
+    # get array of node names
     def nodes
+      # TODO: perhaps we have to ignore entries without "deactivated": null?
       # TODO: in '/v3/nodes' we must take 'name'
       api_nodes.map { |data| data['certname'] }
     end
 
-    def nodes_update_facts_since(timestamp)
-      ts = timestamp
-      ts = Time.iso8601(ts) if ts.is_a?(String)
-      json = api_nodes
-      json.delete_if { |data| Time.iso8601(data["facts_timestamp"]) < ts }
-      # TODO: in '/v3/nodes' we must take 'name'
-      json.delete_if { |data| data["facts_timestamp"] }.map { |data| data['certname'] }
+    # get array of node names
+    def nodes_properties
+      result = {}
+      api_nodes.each do |data|
+        next if data['deactivated']
+        # TODO: in '/v3/nodes' we must take 'name'
+        name = data['certname']
+        values = data.dup
+        %w(deactivated certname).each { |key| values.delete(key) }
+        result[name] = values
+      end
+      result
     end
 
+    # get all nodes that have updated facts
+    def nodes_update_facts_since(timestamp)
+      ts = (timestamp.is_a?(String) ? Time.iso8601(ts) : timestamp)
+      nodes_properties.delete_if do |_k, data|
+        # TODO: in '/v3/nodes' we must take 'facts_timestamp'
+        !data["facts-timestamp"] || Time.iso8601(data["facts-timestamp"]) < ts
+      end.keys
+    end
+
+    # get hash of facts for given node name
     def node_facts(node)
       json = get_json("#{@nodes_url}/#{node}/facts", 10)
       return nil if json.include?("error")
       Hash[json.map { |data| [data["name"], data["value"]] }]
     end
 
-    # get all facts
+    # get all nodes with all facts
     def facts
       json = get_json(@facts_url, 60)
-      # json.each { |fact| pp fact }
       result = {}
       json.each do |fact|
         data = result[fact["certname"]]
@@ -50,11 +66,11 @@ module PuppetDBQuery
       result
     end
 
-    private
-
     def api_nodes
       get_json(@nodes_url, 10)
     end
+
+    private
 
     def get_json(url, timeout)
       @lock.synchronize do
