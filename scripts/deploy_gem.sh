11414ab7279f21fd5ea9c4bdfca454d053f344e9
m31@mymble.moominland <search@meyling.com>
2016-12-28 18:51:38 +0100

    fixed deploy script error

--- scripts/deploy_gem.sh
+++ scripts/deploy_gem.sh
@@ -23,21 +23,21 @@ function puts () {
 
 ## bump version, must be in main git directory
 function bump_version () {
-  puts "bump vampire gem version"
+  puts "bump ${application} gem version"
 
-  old_version=$( ruby -I lib/vampire -e 'require "version"; puts Gem::Version.new(Vampire::VERSION)')
+  old_version=$( ruby -I lib/${application} -e 'require "version"; puts Gem::Version.new(Vampire::VERSION)')
   puts "gem version currently:" ${old_version}
 
-  new_version=$( ruby -I lib/vampire -e 'require "version"; puts Gem::Version.new(Vampire::VERSION + ".1").bump' )
+  new_version=$( ruby -I lib/${application} -e 'require "version"; puts Gem::Version.new(Vampire::VERSION + ".1").bump' )
   puts "we will change it into:" ${new_version}
 
-  cat lib/vampire/version.rb | sed "s/$old_version/$new_version/" > lib/vampire/version.rb.new
-  mv lib/vampire/version.rb.new lib/vampire/version.rb
+  cat lib/${application}/version.rb | sed "s/$old_version/$new_version/" > lib/${application}/version.rb.new
+  mv lib/${application}/version.rb.new lib/${application}/version.rb
 }
 
 ## commit and push version, must be in main git directory
 function commit_and_push_version () {
-  puts "commit and push gem version"
+  puts "commit and push ${application} gem version"
   git add Gemfile.lock lib/${application}/version.rb
   git commit -m "m31's version bumper"
   git push
@@ -61,6 +61,9 @@ bundle install
 puts "check and test"
 bundle exec rake spec
 
+bump_version
+commit_and_push_version
+
 puts "build gem"
 result=$( gem build ${application}.gemspec )
 puts "  ${result}"
@@ -76,9 +79,6 @@ if [ -z "${gem_file}" ]; then
     echo "generated gem file not found" >&2
     exit 1
 fi
-
-bump_version
-commit_and_push_version
 
 puts "push gem to rubygems"
 gem push ${gem_file} --host https://rubygems.org
